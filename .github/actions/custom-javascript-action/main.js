3cabcc958722451ee9713192080c5b2e1fbb729e
Sean Conroy <seanconroy2001@gmail.com>
2024-07-29 11:47:52 +0100

    Add input to javascript workflow

--- .github/actions/custom-javascript-action/main.js
+++ .github/actions/custom-javascript-action/main.js
@@ -1,9 +1,8 @@
 const core = require('@actions/core');
-// const github = require('@actions/github');
-// const exec = require('@actions/exec');
 
 function run() {
-  core.notice("Hello World from a custom javascript action.")
+  const message = core.getInput('message', { required: true });
+  core.notice(message);
 }
 
 run();
